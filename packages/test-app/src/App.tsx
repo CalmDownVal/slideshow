de0ce1383b8c19355c159e6b3317dbbdee626ef1
CalmDownVal <vaclav.lahuta@gmail.com>
2021-09-18 06:31:05 +0200

    remove redundant fragment

--- packages/test-app/src/App.tsx
+++ packages/test-app/src/App.tsx
@@ -6,17 +6,15 @@ import { Transitions } from '~/components/Transitions/Transitions';
 
 export function App() {
 	return (
-		<>
-			<ScrollPresentation>
-				<Viewport scrollable>
-					<Slide component={Transitions} metadata='1' />
-					<Slide component={Transitions} metadata='2' length={0.3} dock={0.5} />
-					<Slide component={Transitions} metadata='3' length={0.3} dock={0.5} />
-					<Slide component={Transitions} metadata='4' length={0.3} dock={0.5} />
-					<Slide component={Transitions} metadata='5' />
-				</Viewport>
-				<Navigation />
-			</ScrollPresentation>
-		</>
+		<ScrollPresentation>
+			<Viewport scrollable>
+				<Slide component={Transitions} metadata='1' />
+				<Slide component={Transitions} metadata='2' length={0.3} dock={0.5} />
+				<Slide component={Transitions} metadata='3' length={0.3} dock={0.5} />
+				<Slide component={Transitions} metadata='4' length={0.3} dock={0.5} />
+				<Slide component={Transitions} metadata='5' />
+			</Viewport>
+			<Navigation />
+		</ScrollPresentation>
 	);
 }
